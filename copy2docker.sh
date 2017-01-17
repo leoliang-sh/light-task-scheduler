b68bdb22cac79bf3b75255837a4af3041d2f86f4
梁志艳 <liangzhiyan@kyee.com.cn>
2017-01-17 14:50:51 +0800

    引入admin的docker配置

---
+++ copy2docker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+VERSION="1.7.0-SNAPSHOT"
+cp -r dist/lts-${VERSION}-bin/war docker/lts-admin/data/
---
+++ docker/lts-admin/build.sh
@@ -0,0 +1,2 @@
+docker build -t registry.cn-hangzhou.aliyuncs.com/ky-edu/lts-admin:latest .
+docker push registry.cn-hangzhou.aliyuncs.com/ky-edu/lts-admin:latest

dfd2cc742b4455c53d2c6f5e88416936e11895a3
梁志艳 <liangzhiyan@kyee.com.cn>
2017-08-18 10:06:53 +0800

    copy2docker.sh提供解压操作

--- copy2docker.sh
+++ copy2docker.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 VERSION="1.7.0"
-cp -r dist/lts-${VERSION}-bin/war docker/lts-admin/data/
+cd dist
+unzip lts-${VERSION}-bin.zip
+cp -r lts-${VERSION}-bin/war ../docker/lts-admin/data/
